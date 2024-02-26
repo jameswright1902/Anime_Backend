d25480c46b6785607bf9200a8e0500ca3ddd72a1
Malik Garvin <67612759+GeecheeCipher@users.noreply.github.com>
2024-02-26 12:15:46 -0500

    Update authentication.js

--- Functionality/authentication.js
+++ Functionality/authentication.js
@@ -1,15 +1,1 @@
-import React, { createContext, useContext } from "react";
 
-const GlobalContext = createContext();
-
-export const GlobalContextProvider = ({ children }) => {
-    return (
-        <GlobalContext.Provider value={'hello'}>
-            {children}
-        </GlobalContext.Provider>
-    )
-}
-
-export const useGlobalContext = () => {
-    return useContext(GlobalContext);
-}
