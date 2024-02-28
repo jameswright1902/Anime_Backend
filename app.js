96fc011308805e7a35626c47fdc886babdd59f62
Malik <malikgarvin76@gmail.com>
2024-02-28 11:05:06 -0500

    userRoutes working

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const express = require("express");
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-
-const router = express.Router();
 const app = express();
 const PORT = 3000;
-
+const userRouter = require("./routes/userRoutes");
 app.listen(PORT, () => {
   console.log(`We are listening on port number ${PORT}`);
 });
@@ -13,32 +9,4 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.send("My first get");
 });
-app.use('/api', require('./api'));
-
-// router.get("/", async (req, res) => {
-//   const page = parseInt(req.query.page) || 1;
-//   const limit = parseInt(req.query.limit) || 10;
-
-//   try {
-//     const offset = (page - 1) * limit;
-//     const users = await prisma.user.findMany({
-//       skip: offset,
-//       take: limit,
-//     });
-
-//     const totalCount = await prisma.user.count();
-//     const totalPages = Math.ceil(totalCount / limit);
-
-//     res.json({
-//       users,
-//       totalCount,
-//       totalPages,
-//       currentPage: page,
-//     });
-//   } catch (error) {
-//     console.error("Error fetching users:", error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// });
-
-module.exports = router;
+app.use("/api", userRouter);
