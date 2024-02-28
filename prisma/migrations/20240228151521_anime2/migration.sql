fe217fa51ffa20e0c872b79fa739609ca8fa852f
jwright1902 <jameswright1902@gmail.com>
2024-02-28 17:22:17 +0100

    added create user table to prisma

---
+++ prisma/migrations/20240228151521_anime2/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT,
+    "password" TEXT,
+    "firstname" TEXT,
+    "lastname" TEXT,
+    "email" TEXT,
+    "admin" BOOLEAN NOT NULL DEFAULT false,
+    "token" TEXT,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_username_key" ON "Users"("username");
