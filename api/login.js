922b60ffa9e0b4efd11397fa8617db1d1f14e895
Malik <malikgarvin76@gmail.com>
2024-03-08 12:10:57 -0500

    update

--- api/login.js
+++ api/login.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // const axios = require('axios');
 const { PrismaClient } = require("@prisma/client");
@@ -11,7 +11,7 @@ const router = express.Router();
 // Initialize Prisma client
 const prisma = new PrismaClient();
 // Route for user login
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     // Destructure username and password from request body
     const { username, password } = req.body;
@@ -23,28 +23,23 @@ router.post('/login', async (req, res) => {
     });
     // If user does not exist, send an error response
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
     // Check if the password matches
     const passwordMatch = await bcrypt.compare(password, user.password);
     // If passwords don't match, send an error response
     if (!passwordMatch) {
-      return res.status(401).json({ error: 'Invalid password' });
+      return res.status(401).json({ error: "Invalid password" });
     }
     // Create JWT token
-    const token = jwt.sign({ userId: user.id }, 'your-secret-key', { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user.id }, "your-secret-key", {
+      expiresIn: "1h",
+    });
     // Send token in response
     res.json({ token });
   } catch (error) {
-    console.error('Error logging in:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error logging in:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 module.exports = router;
-
-
-
-
-
-
-
