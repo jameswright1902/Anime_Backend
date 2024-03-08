ceeae911eba0b8d1c573e34c5544de63d6d9463c
Nick Mack <nickmack44@gmail.com>
2024-03-08 13:27:54 -0500

    removed cors option

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const userRouter = require('./routes/userRoutes');
+const express = require("express");
+const cors = require("cors");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 const PORT = 3000;
 
-// Define CORS options
-const corsOptions = {
-  origin: 'http://localhost:5173', // Allow requests from this origin
-};
-
 // Use CORS middleware with the defined options
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Other middleware setup
 app.use(express.json());
 
 // Mount routes
-app.use('/', userRouter);
+app.use("/", userRouter);
 
 // Start the server
 app.listen(PORT, () => {
